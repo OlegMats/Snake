40a2197238cc6b8a0b8d2edc6d9a95302ce2a612
OlegMats <70022746+OlegMats@users.noreply.github.com>
2020-08-24 22:20:16 +0300

    Add files via upload

---
+++ MainWindow.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class MainWindow extends JFrame {
+
+	public MainWindow() {
+		setTitle("Snake");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		add(new GameField());
+		setSize(350, 370);
+		setLocationRelativeTo(null);
+		setResizable(false);
+//		setLocation(800,300); // позиционирование на экране; 
+		setVisible(true);
+	}
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow();
+	}
+
+}
